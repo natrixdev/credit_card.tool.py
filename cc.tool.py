9c6ade16d30ce852da012b29d05329e7825da281
natrix_dev <discordlenko@gmail.com>
2022-06-04 10:41:47 +0200

    Create cc.tool.py

---
+++ cc.tool.py
@@ -0,0 +1,33 @@
+import [cc, pystyle, requests, tryer, er, os, time, math, error, whilex, x, y, github_stats]
+
+try:
+  imports;
+    return;
+  
+ x = (f""" 
+                            Credit Card 
+                               Tool
+                                      > By natrix
+
+ """
+      
+ y = input('Type a name to find his credit card')
+      
+ 
+ for y:
+      try(pystyle).er
+      while y __NOT FOUND__  > 0 :
+        time.sleep(0.01)
+      os.print(err)
+   cc.generate(name=y, numbers=requests('https://cc-finder.{y}:format==pptx')
+               math.calc .binary()
+                &&print('Credit Card numbers for {y}'):
+             cc();
+          breack(p)
+      exit()
+               
+               powershell.return('QCreate')->file('cc.txt)+date
+                                                 
+   qsheep()
+                                                  
+                                                  a.ml
